3d530a09a958025cb9e086d447ee0d5e9c746696
Young <90114927+ImYoung-Geneloop@users.noreply.github.com>
2023-02-22 09:35:22 -0500

    Delete handlers directory

--- handlers/database.js
+++
@@ -1,7 +0,0 @@
-const { MongoDBConnector } = require('discord.js-v14-helper');
-
-module.exports = (client, config) => {
-    const connector = new MongoDBConnector(require('../config/main').database.mongodb_uri);
-    
-    connector.start();
-};
